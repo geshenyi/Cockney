e5cf5fd96f7809690b25839aefddd314ce2234f1
Chuck Ge <ssge@ebay.com>
2014-09-22 21:26:56 +0800

    commit relevant .js and .css

---
+++ public/javascripts/trigger-job.js
@@ -0,0 +1,6 @@
+/**
+ * Created by ssge on 2014/9/22.
+ */
+function triggerJob(){
+    alert('triggered');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var http = require('http');
+var rest = require('../node_modules/restler');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/trigger', function(req,res){
+    console.log('in trigger');
+    rest.post("http://int.testing.stubcorp.dev/jenkins/job/TestJenkinsApi/buildWithParameters",{data:{token:'testapi',testStringParam:'triggerNode'}}).on('complete',function(data,response){
+        console.log(response.statusCode);
+    });
+    res.json({"message":"test"});
 });
 
 module.exports = router;
