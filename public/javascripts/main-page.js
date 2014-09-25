01e0c2d67eab319453668c7a98df6103088a61d4
Chuck Ge <ssge@ebay.com>
2014-09-25 10:33:06 +0800

    output info in main-page

--- public/javascripts/main-page.js
+++ public/javascripts/main-page.js
@@ -1,10 +1,16 @@
 /**
  * Created by ssge on 2014/9/22.
  */
-function triggerJob(){
-    alert('triggered');
+function outputTextInfo(){
+    console.log(document.querySelector('paper-input#major').value);
+    console.log($('#major')[0]);
+    console.log(document.querySelector('paper-input#major'));
+    console.log(document.querySelector('paper-input#minor').value);
+    console.log(document.querySelector('paper-radio-group#runserver').selected);
+
+
 }
 
 $(document).ready(function(){
-    alert('ready');
+
 });
