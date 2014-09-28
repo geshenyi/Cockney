7f801bfc6fc9dec2c3d32e91babc3b00789ea21f
Chuck Ge <ssge@ebay.com>
2014-09-28 13:38:54 +0800

    main page script update

--- public/javascripts/main-page.js
+++ public/javascripts/main-page.js
@@ -11,6 +11,68 @@ function outputTextInfo(){
 
 }
 
-$(document).ready(function(){
+function settingClicked(job){
+    alert(job);
+}
 
+function changeEvent(){
+    alert('a');
+}
+
+function toggleDialog(id){
+    var dialog = document.querySelector('#'+id);
+    dialog.toggle();
+}
+
+$(document).ready(function(){
+    var tabs = document.querySelector('paper-tabs');
+//    tabs.addEventListener('core-select',function(event){
+//        if(event.detail.isSelected && tabs.selected == 1){
+//            console.log(tabs.selected);
+//            var form1 = $('#configForm');
+//            form1.attr('action','/history');
+//            form1.submit();
+//        }
+//
+//    });
+    var historyTab = document.querySelector('#historyTab');
+    var runningTab = document.querySelector('#runningTab');
+    var buyapiButton = document.querySelector('#buyapi');
+    var usuiSetting = document.querySelector('#usui_setting');
+    var usuiSave = document.querySelector('#usui_save');
+
+    buyapiButton.addEventListener('click',function(e){
+        alert('buy api trigger');
+    });
+    historyTab.addEventListener('click',function(e){
+        var form1 = $('#configForm');
+            form1.attr('action','/history');
+            form1.submit();
+    });
+    runningTab.addEventListener('click',function(e){
+        var form1 = $('#configForm');
+        form1.attr('action','/running');
+        form1.submit();
+    });
+//    usuiSetting.addEventListener('click',function(e){
+//        $('#usui_front').css('display','none');
+//        $('#usui_back').css('display','block');
+//        $('#usui_setting').addClass('hide');
+//        $('#usui_save').removeClass('hide');
+//    })
+//
+//    usuiSave.addEventListener('click',function(e){
+//        $('#usui_front').css('display','block');
+//        $('#usui_back').css('display','none');
+//        $('#usui_setting').removeClass('hide');
+//        $('#usui_save').addClass('hide');
+//    })
 });
+
+function testv1Clicked(){
+    $('#eventidv1').toggleClass('hide');
+}
+
+function testv2Clicked(){
+    $('#eventidv2').toggleClass('hide');
+}
